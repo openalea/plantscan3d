e7d437f23d8270b049de5361f4b1f8f83c064cfd
Frederic Boudon <fredboudon@users.noreply.github.com>
2018-11-07 17:06:26 +0100

    add the ui and qrc file

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     # (you can provide an exclusion dictionary named exclude_package_data to remove parasites).
     # alternatively to global inclusion, list the file to include   
     #package_data = {'' : ['*.pyd', '*.so'],},
-
+    package_data = {'' : ['*.pyd', '*.so', '*.dylib', '*.lpy','*.ui','*.qrc'],},
     # postinstall_scripts = ['',],
 
     # Declare scripts and wralea as entry_points (extensions) of your package 
